2234e995e88a688db640853a92fbbcb13985fbe8
fllesser <fllessive@gmail.com>
2025-08-27 14:15:00 +0800

    🎈 perf: 优化抽象逻辑

--- src/nonebot_plugin_abs/data_source.py
+++ src/nonebot_plugin_abs/data_source.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 import re
 
 import jieba
@@ -16,60 +17,58 @@ async def init_jieba():
 
 def text2emoji(text: str) -> str:
     word_lst: list[str] = jieba.lcut(text)
-    emoji_str = ""
+    emj_lst: list[str] = []
     for word in word_lst:
         if bool(re.fullmatch(r"^[a-zA-Z0-9]+$", word)):
-            emoji_str += en2emoji(word)
+            emj_lst.extend(en2emoji(word))
         else:
-            emoji_str += zh2emoji(word)
+            emj_lst.extend(zh2emoji(word))
 
-    return emoji_str
+    return "".join(emj_lst)
 
 
-def en2emoji(en_num: str) -> str:
+def en2emoji(en_num: str) -> Sequence[str]:
     if emj := en_emj_map.get(en_num):
         logger.debug(f"[en] 英文 {en_num} -> {emj}")
         return emj
 
-    elif emj := py_emj_map.get(en_num):
+    if emj := py_emj_map.get(en_num):
         logger.debug(f"[en] 拼音 {en_num} -> {emj}")
         return emj
 
-    else:
-        emjs = ""
-        for char in en_num:
-            if char.isdigit() and (emj := num_emj_map.get(char)):
-                emjs += emj
-                logger.debug(f"[en] 数字 {char} -> {emj}")
-            else:
-                emjs += char
-                logger.debug(f"[en] 忽略 {char}")
-        # logger.debug(f"[en] 其他 {en_num} -> {emjs}")
-        return emjs
+    emj_lst: list[str] = []
+    for char in en_num:
+        if char.isdigit() and (emj := num_emj_map.get(char)):
+            emj_lst.append(emj)
+            logger.debug(f"[en] 数字 {char} -> {emj}")
+        else:
+            emj_lst.append(char)
+            logger.debug(f"[en] 忽略 {char}")
+    # logger.debug(f"[en] 其他 {en_num} -> {emjs}")
+    return emj_lst
 
 
-def zh2emoji(zh: str) -> str:
+def zh2emoji(zh: str) -> Sequence[str]:
     if emj := zh_emj_map.get(zh):
         logger.debug(f"[zh] 中文 {zh} -> {emj}")
         return emj
 
-    elif (zh_py := pinyin.get(zh, format="strip")) and (emj := py_emj_map.get(zh_py)):
+    if (zh_py := pinyin.get(zh, format="strip")) and (emj := py_emj_map.get(zh_py)):
         logger.debug(f"[zh] 拼音 {zh} -> {zh_py} -> {emj}")
         return emj
 
-    else:
-        if len(zh) == 1:
-            logger.debug(f"[zh] 忽略 {zh}")
-            return zh
+    if len(zh) == 1:
+        logger.debug(f"[zh] 忽略 {zh}")
+        return zh
 
-        emjs = ""
-        for char in zh:
-            char_py = pinyin.get(char, format="strip")
-            if emj := py_emj_map.get(char_py):
-                emjs += emj
-                logger.debug(f"[zh] 拼音 {char} -> {char_py} -> {emj}")
-            else:
-                emjs += char
-                logger.debug(f"[zh] 忽略 {char}")
-        # logger.debug(f"[zh] 单合 {zh} -> {emjs}")
-        return emjs
+    emj_lst: list[str] = []
+    for char in zh:
+        char_py = pinyin.get(char, format="strip")
+        if emj := py_emj_map.get(char_py):
+            emj_lst.append(emj)
+            logger.debug(f"[zh] 拼音 {char} -> {char_py} -> {emj}")
+        else:
+            emj_lst.append(char)
+            logger.debug(f"[zh] 忽略 {char}")
+    # logger.debug(f"[zh] 单合 {zh} -> {emjs}")
+    return emj_lst
